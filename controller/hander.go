69d52511fe5264ad3542efe76aca556d8e912b20
zhoumin12 <1966233584@qq.com>
2022-03-13 22:15:20 +0800

    优化代码结构2

--- controller/hander.go
+++ controller/hander.go
@@ -8,10 +8,10 @@ import (
 
 func Init() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+	r.GET("/parkinginfo", func(c *gin.Context) {
 		info := model.GetParkingInfo()
 		//c.String(http.StatusOK, info)
-		c.JSON(http.StatusOK, gin.H{"code": "200", "data": info})
+		c.JSON(http.StatusOK, gin.H{"code": 200, "data": info})
 	})
 	r.Run(":8080")
 }
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "car-golang/controller"
+import (
+	"car-golang/controller"
+	"fmt"
+)
 
 func main() {
+	fmt.Println("开始启动")
 	controller.Init()
+	fmt.Println("启动成功")
 }
