c4c5e30e778aa6b58f7bcd1608f9e4819a645f6e
zhoumin12 <1966233584@qq.com>
2022-03-13 19:59:33 +0800

    测试连接

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	// 2.绑定路由规则，执行的函数
 	// gin.Context，封装了request和response
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello World!")
+		c.String(http.StatusOK, "hello 傅文杰 你是傻子吗!")
 	})
 	// 3.监听端口，默认在8080
 	// Run("里面不指定端口号默认为8080")
